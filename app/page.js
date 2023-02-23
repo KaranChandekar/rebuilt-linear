136124f149b03b045a246abb0a0b5e0568e035d1
Karan Chandekar <karanchandekar431@gmail.com>
2023-02-23 21:08:54 +0530

    header & main navigation component

--- app/page.js
+++ app/page.js
@@ -3,28 +3,18 @@ import { Hero, HeroSubtitle, HeroTitle } from '@/components/Hero';
 
 function Home() {
   return (
-    <div>
-      <header>
-        <Container>header</Container>
-      </header>
-      <main>
-        <Container>
-          <Hero>
-            <HeroTitle>
-              Linear is a better way <br /> to build products
-            </HeroTitle>
-            <HeroSubtitle>
-              Meet the new standard for modern software development. <br />
-              Streamline issues, sprints, and product roadmaps.
-            </HeroSubtitle>
-            <img src="/img/hero.jpeg" alt="Hero image" />
-          </Hero>
-        </Container>
-      </main>
-      <footer>
-        <Container>footer</Container>
-      </footer>
-    </div>
+    <Container>
+      <Hero>
+        <HeroTitle>
+          Linear is a better way <br /> to build products
+        </HeroTitle>
+        <HeroSubtitle>
+          Meet the new standard for modern software development. <br />
+          Streamline issues, sprints, and product roadmaps.
+        </HeroSubtitle>
+        <img src="/img/hero.jpeg" alt="Hero image" />
+      </Hero>
+    </Container>
   );
 }
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       sans: `"Linear","SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu, Cantarell,"Open Sans","Helvetica Neue",sans-serif`,
     },
     fontSize: {
+      xs: '1.3rem',
+      sm: '1.4rem',
       md: '1.6rem',
       lg: ['2.2rem', '1.3'],
       '5xl': ['8rem', 1],
@@ -17,6 +19,7 @@ module.exports = {
     colors: {
       background: '#000212',
       white: '#FFFFFF',
+      'white-a08': 'rgba(255, 255, 255, 0.08)',
     },
     spacing: {
       0: '0',
@@ -32,6 +35,10 @@ module.exports = {
       10: '4rem',
       11: '4.4rem',
       12: '4.8rem',
+    },
+    backgroundImage: {
+      'primary-gradient':
+        'linear-gradient(92.88deg,#455eb5 9.16%,#5643cc 43.89%,#673fd7 64.72%)',
     },
   },
   plugins: [],
