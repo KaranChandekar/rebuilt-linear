abfef637e05d87ff5ea3297271a7ad6c17e14d94
Karan Chandekar <karanchandekar431@gmail.com>
2023-02-24 17:23:52 +0530

    reset padding

--- components/Container.jsx
+++ components/Container.jsx
@@ -2,7 +2,12 @@ import classNames from 'classnames';
 
 const Container = ({ children, className }) => {
   return (
-    <div className={classNames('max-w-[120rem] px-4 md:px-0 mx-auto', className)}>
+    <div
+      className={classNames(
+        'max-w-[120rem] px-4 md:px-8 xl:px-0 mx-auto',
+        className
+      )}
+    >
       {children}
     </div>
   );
