b7074955247a28cb20f48d84f99b08b3286bebab
Karan Chandekar <karanchandekar431@gmail.com>
2023-02-25 21:43:15 +0530

    update

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Rebuilt Linear - A better way to build products</title>
+      <meta
+        name="description"
+        content="Rebuilding Linear.app's website with Next.js, Tailwind."
+      />
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
